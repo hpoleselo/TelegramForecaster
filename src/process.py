71eb63e3424281ee87fb2b1ee56fc39679189ca4
Henrique Poleselo <hpoleselo@gmail.com>
2020-07-07 00:06:50 -0300

    Appending dates correctly according to the first day count

--- src/process.py
+++ src/process.py
@@ -1,69 +1,92 @@
 import pandas as pd
 import forecast
 
-#testar criacao de dataframe com for
-#for i in range(3)
-
-#forecastTable = {'Sun-20'}
-
-#forecastTable = {'day1': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
-#        'day12': [22000,25000,27000,35000]
-#        }
-
-#index=['Horário','Período:','Velocidade do Vento:','Direção do Vento:'] #,'Tamanho da Ondulação:','Direção da Ondulação:', 'Energia da Ondulação:']
-#columns =  ['day1','day12']
-#columns =  ['day1','day12','day13']
-#df = pd.DataFrame(forecastTable, columns = columns, index=index)
-#energy.max() pra pegar o dia com a maior energia ou o dia com o menor vento? fazer um matching?
-
-#print(df)
 
 def processData(period, wind_speed, wind_direction, wave_height, wave_direction, energy, dates, time):
     """ All the data retrieved is gonna be treated now to be sent to Telegram
     Send as text? Generate table using Pandas but then converting to csv style?
     """
-    print(time)
-    if time[0] == "6PM":
-        for i in range(2):
-            print("Gera 2 datas")
 
-    # IDENTIFICAMOS AS TRANCISOES DOS DIAS (A TRANSICAO EH DE 9PM P 0AM NA TABELA)
-    # CONTAMOS QTO LEVA DO PRIMEIRO TERMO ATE O TERMO QUE POSSUI A TRANSICAO DE 9PM P AM
-    # USAREMOS 2 FOR, O PRIMEIRO PRA UM CASO GERAL CASO HAJA TRANSICAO AM->PM E O SEGUNDO PRA CHECAGEM FINA DE EH DE 
+    initialColumns = checaDeslocamento(time)
 
-    checaDeslocamento(time)
+    # Creating our pandas dataframe
+    forecastTable = {}
+    columns = []
+    counter = 0
+    # 8 is the number of subdivisions in a day
+    secondThreshold = initialColumns + 8
+    thirdThreshold = secondThreshold + 8
+    #fourthThreshold = 
 
+    index = ['Horário','Período da Ondulação:','Velocidade do Vento:','Direção do Vento:', 'Tamanho da Ondulação:','Direção da Ondulação:', 'Energia da Ondulação:']
+    
+    # Colocar len
+    for i in range(23):
+        counter+=1
+        if counter <= initialColumns:
+            columns.append(dates[0])   
+        elif counter <= secondThreshold:
+            columns.append(dates[1])
+        elif counter <= thirdThreshold:
+            columns.append(dates[2])
+        elif counter > thirdThreshold:
+            columns.append(dates[3])
+
+
+    df = pd.DataFrame(forecastTable, columns=columns, index=index)
+    print(df)
+    # pegar max.energy() p/ avaliar melhor dia com min.wind()
     # Using pandas, try to create a table from this csv and then generate image/text?
 
 
+def generateImage(df):
+    pass
+    #return image
+
+
 def checaDeslocamento(time):
-    # NA VERDADE EH MAIS FACIL AINDA, SO CHECAR A TRANSICAO PM->AM, SO OCORRE QUANDO PASSA DE UM DIA PRO OUTRO!!!
-    # dependendo do numero de casas , geramos o numero inicial a partir desse contador e ai colocamos os dias x vezes no forecastable
-    firstCounter = 0
-    secondCounter = 0
+    """ Since the day the we're looking isn't going to display the full day (all the eight times), then we have to know before how many
+    days we're going to plot since it's not 8. We do that by checking the transition between PM -> AM (when it's the transition between days) 
+    
+    The variable returned here is the number of columns with the first day to be plotted.
+    """
+    pmCounter = 0
+    amCounter = 0
     generalCounter = 0
-    initialState = time[0]
+    initialTime = time[0]
+    initialState = initialTime[-2:]
     for i in range(len(time)):
         generalCounter += 1
-        currentState = time[i]
-        # -2: gets the PM/AM
-        if currentState[-2:] == "PM":
-            print("Current state is PM")
-            firstCounter += 1
-            print("\nFirst counter: ", firstCounter)
-        elif currentState[-2:] == "AM":
-            print("Current state is AM")
-            secondCounter += 1
-            print("\nSecond counter: ", secondCounter)
-        if not firstCounter < generalCounter:
-            print("Chegou a transicao pois o contador geral ultrapassou o contador local do PM")
-            break
-    print("Contador global: ", generalCounter)
-    print("Contador local 1: ", firstCounter)
-    # Because the counting begins on 0.. So we're gonna place 2 spots if the generalCounter reutnrrs 1
-    displacement = generalCounter + 1
+        currentTime = time[i]
+        # Since we want to detect the transition from PM to AM, then we just have to check when this transiion happens.
+        # -2: gets the PM/AM suffix from the time
+        currentState = currentTime[-2:]
 
-    # test tomorrow for other cases and check if the number of initial pms on the list matches the displacement
+        if currentState == "AM":
+            amCounter += 1
+
+        if not (currentState == initialState):
+            print("Geral: Houve transição de horário!")
+            if (initialState == "PM"):
+                print("\nCaso o estado seja PM:")
+                print("Fez a transicao de dias!")
+                print("Plotar: ", generalCounter-1)
+            else:
+                print("\nCaso o estado seja AM:")
+                print("Não houve transição de dias")
+                print("Plotar horarios pela manhã: ", amCounter)
+                print("Plotar horarios PM: 4")
+            break
+
+    # Since we detect the transition after n+1 iterations, we have to decrease by 1
+    displacement = generalCounter - 1
+
+    """
+    if (pmCounter < amCounter):
+        print("Quer dizer que a transicao foi ..")
+    """
+
+    return displacement
 
 def main():
     response = forecast.checkPage()
