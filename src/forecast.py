d6bd994b3b66d245b01e1f946d12acad51262b44
Henrique Poleselo <hpoleselo@gmail.com>
2021-05-14 19:52:59 -0300

    Added stop criteria to limit the number of columns, but data and time lists are still buggy.

--- src/forecast.py
+++ src/forecast.py
@@ -53,8 +53,9 @@ def getContent(pageText):
 	low_tide = []
 	dates = []
 	time = []
-	# Variable added for our application to be more scalable, I have figure out somehow how to get the limit instead of hardcoding it
-	table_length = 60
+
+	# Variable added for our application to be more scalable, I have to figure out somehow how to get the limit instead of hardcoding it
+	table_length = 36
 
 
 	# TODO: Checar o porquê o tamanho de time eh 46 e nao 23, ele ta dando parsing 2x?
@@ -66,66 +67,103 @@ def getContent(pageText):
 	# find_all retorna um iteravel, por isso precisamos passar por um for
 
 	# Wave period
+	counterStop = 0
 	forecastTableRows = soup.find_all("tr", {"data-row-name":"periods"})
 	for tableRow in forecastTableRows:
 		tableCells = tableRow.find_all('td', class_='forecast-table__cell')
 		for cell in tableCells:
+			counterStop += 1
 			value = cell.find("strong").text
 			period.append(value)
+			if counterStop == table_length:
+				break
+
 
 	# Wave energy
+	counterStop = 0
 	forecastTableRows = soup.find_all("tr", {"data-row-name":"energy"})
 	for tableRow in forecastTableRows:
 		tableCells = tableRow.find_all('td', class_='forecast-table__cell')
 		for cell in tableCells:
+			counterStop += 1
 			value = cell.find("strong").text
 			energy.append(value)
+			if counterStop == table_length:
+				break
 
 	# Wind speed and direction
+	counterStop = 0
 	forecastTableRows = soup.find_all("tr", {"data-row-name":"wind"})
 	for tableRow in forecastTableRows:
 		# Gets the wind direction
 		tableCells = tableRow.find_all('div', class_='forecast-table__value')
 		for cell in tableCells:
+			counterStop += 1
 			wind_direction.append(cell.text)
+			if counterStop == table_length:
+				break
 
 	# Gets the wind speed
+	counterStop = 0
 	windSpeeds = tableRow.find_all('text', class_='wind-icon-val')
 	for windSpeed in windSpeeds:
+		counterStop += 1
 		wind_speed.append(windSpeed.text)
+		if counterStop == table_length:
+				break
 
 
 	# High-tide 
+	counterStop = 0
 	forecastTableRows = soup.find_all("tr", {"data-row-name":"high-tide"})
 	for tableRow in forecastTableRows:
 		tableCells = tableRow.find_all('div', class_='forecast-table__value--tiny')
 		for cell in tableCells:
+			counterStop += 1
 			high_tide.append(cell.text)
+			if counterStop == table_length:
+				break
 
 	# Low-tide
+	counterStop = 0
 	forecastTableRows = soup.find_all("tr", {"data-row-name":"low-tide"})
 	for tableRow in forecastTableRows:
 			lowTides = tableRow.find_all('div', class_='forecast-table__value--tiny')
 			for lowTide in lowTides:
+					counterStop += 1
 					low_tide.append(lowTide.text)
+					if counterStop == table_length:
+						break
 
 
-	# Wave height
+	# Wave Direction
+	counterStop = 0
 	forecastTableRows = soup.find_all("tr", {"data-row-name":"wave-height"})
 	for tableRow in forecastTableRows:
 		tableCells = tableRow.find_all('div', class_='forecast-table__value')
 		for cell in tableCells:
+			counterStop += 1
 			wave_direction.append(cell.text)
+			if counterStop == table_length:
+				break
 
+	# Wave Height
+	counterStop = 0
 	waveHeights = tableRow.find_all('text', class_='swell-icon-val')
 	for waveHeight in waveHeights:
+		counterStop += 1
 		wave_height.append(waveHeight.text)
-
+		if counterStop == table_length:
+				break
 
 	# Time
+	counterStop = 0
 	forecastTableRows = soup.find_all("tr", {"data-row-name":"time"})
 	for tableRow in forecastTableRows:
 		tableCells = tableRow.find_all('td', class_='forecast-table-time__cell')
+		counterStop += 1
+		if counterStop == table_length:
+				break
 		for cell in tableCells:
 			timeOfTheDay = cell.text
 			# One of the characters wasn't in Unicode, so either we had to eliminate the Unicode part (which in my case was useless)
@@ -133,13 +171,16 @@ def getContent(pageText):
 			timeOfTheDayConv = (timeOfTheDay.encode('ascii', 'ignore')).decode("utf-8")
 			time.append(timeOfTheDayConv)
 
-
 	# Date/Days of the week
+	counterStop = 0
 	forecastTableRows = soup.find_all("tr", {"data-row-name":"days"})
 	for tableRow in forecastTableRows:
 		tableCells = tableRow.find_all("div", {"class":'forecast-table__value'})
 		for cell in tableCells:
+			counterStop += 1
 			dates.append(cell.text)
+			if counterStop == table_length:
+				break
 
 
 	"""
@@ -158,18 +199,20 @@ def getContent(pageText):
 	# ! Data Treatment !
 
 	# When we parse the wind direction we get 'Vento' and 'km/h' as values, so we slice them out from the list!
-	wind_direction = wind_direction[2:]
+	#wind_direction = wind_direction[2:]
 
 	# Was getting 46 for the length (somehow it is parsing twice? Check later.)
 	# UPDATE: It always retrieves time twice, after the Surfforecast update it went from 23 to 60 time positions
 	# Meaning we have to slice it to 60.
-	time = time[:table_length]
+	#time = time[:table_length]
 
 	# Days were being repeated
-	dates_temp = dates[:12]
+	dates_temp = dates[:26]
 	# Cleanup the list
 	dates = []
 	remainder = 0
+	
+	# We do this because the list comes in a strange way so we have to clean it up!
 	# Get values Sabado20, Domingo21... (Divisible by 3 including the first one (0))
 	for i in range(len(dates_temp)):
 		if i == 0:
@@ -178,7 +221,9 @@ def getContent(pageText):
 			dates.append(dates_temp[i])
 
 	if verbose:
-		print("## Resultados ##\n")
+		print("----- Results -----\n")
+		print("\nHorario: ", time)
+		print("Tamanho Horários: ", len(time))
 		print("\nPeriodo das ondas: ", period)
 		print("Tamanho: ", len(period))
 		print("\nEnergia das ondas: ", energy)
@@ -197,8 +242,6 @@ def getContent(pageText):
 		print("Tamanho: ", len(low_tide))
 		print("\nDatas: ", dates)
 		print("Tamanho: ", len(dates))
-		print("\nHorários: ", time)
-		print("Tamanho: ", len(time))
     # Use dictionary to return since there're too many values?
 	return table_length, period, wind_speed, wind_direction, wave_height, wave_direction, energy, dates, time, high_tide, low_tide
 		
